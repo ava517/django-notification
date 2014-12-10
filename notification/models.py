cafa6e6fca7a1f22300921eb0ec3525db6893f69
sawidis <savvidis@transifex.com>
2014-12-10 20:05:20 +0200

    create_notice_type initializes is_visible, too

--- notification/models.py
+++ notification/models.py
@@ -162,7 +162,8 @@ class Notice(models.Model):
     get_absolute_url = models.permalink(get_absolute_url)
 
 
-def create_notice_type(label, display, description, default=2, verbosity=1):
+def create_notice_type(label, display, description, is_visible,
+                       default=2, verbosity=1):
     """
     Creates a new NoticeType.
 
@@ -180,12 +181,18 @@ def create_notice_type(label, display, description, default=2, verbosity=1):
         if default != notice_type.default:
             notice_type.default = default
             updated = True
+        if is_visible != notice_type.is_visible:
+            notice_type.is_visible = is_visible
+            updated = True
         if updated:
             notice_type.save()
             if verbosity > 1:
                 print "Updated %s NoticeType" % label
     except NoticeType.DoesNotExist:
-        NoticeType(label=label, display=display, description=description, default=default).save()
+        NoticeType(
+            label=label, display=display, description=description,
+            default=default, is_visible=is_visible
+        ).save()
         if verbosity > 1:
             print "Created %s NoticeType" % label
 
